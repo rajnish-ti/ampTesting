939673b93dec293507bd736183236c8564e1f66d
rajnish-ti <102950825+rajnish-ti@users.noreply.github.com>
2023-08-16 17:35:22 +0530

    Update testworker.js

--- testworker.js
+++ testworker.js
@@ -1,1 +1,18 @@
-console.log("I'm Working");
+fetch("https://nbtfeed.speakingtree.in/langapi/masterauthor", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json;charset=utf-8",
+                    },
+                })
+                .then(response => response.json()) // This returns a promise
+                .then(responseData => {
+                    const authors = responseData && responseData.authors;
+                    let authorList = ``;
+                    authors.map(item => {
+                        authorList += `<div><h1>${item.name}</h1></div>`;
+                    })
+                    document.body.innerHTML = authorList;
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
